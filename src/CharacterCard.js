ca6d5de6d0e994a02e438b7a8d74544d86ad60ab
Teeraphat <teeraphat123@hotmail.com>
2019-09-14 15:37:41 +0700

    add Components & States Swap

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
 
-export default class CharacterCard extends React.Component{
-    constructor(props){
+export default class CharacterCard extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
+        this.state = {
             active: false,
         }
     }
 
-    activate = () =>{
-        let activeName= this.state.active ? 'activeCard':'';
-        let className = `card${activeName}`;
+    activate = () => {
+        if (!this.state.active) {
+            this.setState({ active: true})
+        }
+    }
+    render() {
+        let activeClass=this.state.active ? 'activeCard' : '';
+        let className = `card ${activeClass}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
 
-        return(
-
-        <div className={className} onClick={this.activate}>
-            {this.props.value}
-        </div>
-        );
     }
 }
