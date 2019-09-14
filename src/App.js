78582503a8000e897738fb5cb308d50ee4034054
Teeraphat <teeraphat123@hotmail.com>
2019-09-14 12:25:28 +0700

    Hello word

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        Hello World
-  </div>
+class App extends React.Component{
+  render(){
+    return(
+      <div class="App">
+        Hello {"World"}
+      </div>
     );
   }
 }
-
+ 
 
 export default App;
